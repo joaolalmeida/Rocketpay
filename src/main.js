eb7a459f7793a0693edf0061237b804b5f77781d
Joao Almeida <joaolalmeida20@gmail.com>
2022-10-18 22:18:33 -0300

    Adicionando expressões regulares

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -13,6 +14,7 @@ const colors = {
   "default": ["black", "gray"]
 }
 
+
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
@@ -26,3 +28,38 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2), //Slice = Fatiar para pegar os dois ultimos digitos do ano atual
+      to: String(new Date().getFullYear() + 10).slice(2) // Colocando + 10 anos no ano atual
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from:1,
+      to:12
+    }
+  }
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: "0000 0000 0000 0000"
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
---
+++ src/regra_cartao.js
@@ -0,0 +1,14 @@
+// visa ^4\d{0,15} - inicia com 4 seguido de um dígito que pode se repetir de 0 a 15
+//inicia com 4 seguido de mais 15 dígitos
+4234234423432344
+
+//master (^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+//usar parenteses para inserir o simbolo '|' para informar 'OU'
+//inicia com 5, seguido de um dígito entre 1 e 5, seguido de mais 2 dígitos
+//OU
+// inicia com 22, seguido de um dígito entre 2 e 9, seguido de mais 1 dígito
+//OU
+// inicia com 2, seguido de um dígito entre 3 e 7, seguido de mais 2 dígitos
+// seguido de mais 12 dígitos
+5353535353535353
+2323232323232323
